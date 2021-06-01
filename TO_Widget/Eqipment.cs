60f3213c1b48584d17750979841d036acb282a4c
Krik Benya <magneto2@mail.ru>
2021-06-01 18:59:47 +0300

    Додано два класа Eqipment i Smena. Розробити алгоритм вираховування тривалості змінию

---
+++ TO_Widget/Eqipment.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TO_Widget
+{
+    [Serializable]
+    public class Eqipment
+    {
+        
+        public string EqipType { get; private set; }
+        public string EqipName { get; private set; }
+        public string EqipInfo { get; private set; }
+        public bool WeekInsp { get; private set; }
+        public bool MonthInsp { get; private set; }
+        public DateTime LastWeekInsp { get; private set; }
+        public DateTime LastMonthInsp { get; private set; }
+        public string WhoDidInsp { get; private set; }
+
+
+        public Eqipment()
+        {
+
+        }
+        public Eqipment(string eqipType, string eqipName,bool weekInsp = true,bool monthInsp = true)
+        {
+            EqipType = eqipType;
+            EqipName = eqipName;
+            EqipInfo = "Інформація відсутня";
+            WeekInsp = weekInsp;
+            MonthInsp = monthInsp;
+            LastWeekInsp = new DateTime();
+            LastMonthInsp = new DateTime();
+            WhoDidInsp = "Інформація відсутня";
+
+        }
+
+
+
+
+    }
+}
---
+++ TO_Widget/Smena.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TO_Widget
+{
+    [Serializable]
+    public class Smena
+    {
+        public string LabelChange { get; private set; }
+        public DateTime DurationChange { get; private set; }
+        public int AmountChange { get; private set; }
+        public DateTime BeginChange { get; set; }
+
+
+        public Smena()
+        {
+
+        }
+        public Smena(string labelChange, int amountChange, int durationHour,int durationMin, int beginHour, int beginMin)
+        {
+            DurationChange = new DateTime(0000, 01, 01, durationHour, durationMin, 00);
+            LabelChange = labelChange;
+            AmountChange = amountChange;
+            BeginChange = new DateTime(0000,01,01,beginHour, beginMin,00);
+        }
+
+
+        public DateTime WhenEndChange()
+        {
+            TimeSpan endChange;
+            
+            DateTime actualTime = DateTime.Now;
+
+            DateTime beginChange = new DateTime(actualTime.Year, actualTime.Month, actualTime.Day, BeginChange.Hour, BeginChange.Minute, 00);
+
+            
+
+            do
+            {
+                endChange = beginChange.AddMinutes(510) - actualTime;
+
+            } while (endChange.Minutes>510 );
+
+
+
+            
+
+
+            return endChange;
+        }
+
+
+
+    }
+}
